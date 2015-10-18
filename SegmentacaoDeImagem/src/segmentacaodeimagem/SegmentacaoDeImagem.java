a6a7333b18a9c3e52f7c3e998a7378e6455d6fe4
Hiago Miguel <miguel.hiago@gmail.com>
2015-10-18 19:31:21 -0300

    Obtendo coordenadas dos pixels

--- SegmentacaoDeImagem/src/segmentacaodeimagem/SegmentacaoDeImagem.java
+++ SegmentacaoDeImagem/src/segmentacaodeimagem/SegmentacaoDeImagem.java
@@ -9,14 +9,11 @@ import br.ufrn.imd.lp2.imagesegmentation.ImageInformation;
 import br.ufrn.imd.lp2.imagesegmentation.ImageSegmentation;
 import java.awt.Color;
 import java.awt.FlowLayout;
-import java.util.ArrayList;
 import java.util.Arrays;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.image.BufferedImage;
-
-
 
 /**
  *
@@ -27,11 +24,6 @@ public class SegmentacaoDeImagem {
     /**
      * @param args the command line arguments
      */
- 
-    
-    public SegmentacaoDeImagem() {
-
-    }
 
  /*  
     public static ImageInformation printLabelsMap(ImageInformation image) {
@@ -39,56 +31,48 @@ public class SegmentacaoDeImagem {
     }
  */
     public static void main(String[] args) {
+                        
         // Segmentação com parâmetros 0.99, 40 e 1000
         ImageInformation seg = ImageSegmentation.performSegmentation("imgs/imd.jpg", 0.99,40,1000);
-        BufferedImage image = seg.getRegionMarkedImage();
+        BufferedImage image = seg.getRegionMarkedImage(); // imagem para testar o mapa de rótulos
+        
+        int height = seg.getRegionMarkedImage().getHeight(); // altura da imagem
+        int width = seg.getRegionMarkedImage().getWidth(); // largura da imagem
+  
+        int[] pixel; // array que contém as coordenadas x,y e o valor rgb do pixel.
+        
+        Color grey = new Color(121, 121, 121); // uma cor para teste
+        int rgb = grey.getRGB();
 
         // Impressão na tela da quantidade de regiões gerada
         System.out.println("Total de regiões: " + seg.getTotalRegions());
         System.out.println( seg.getRegionMarkedPixels().length + "Pixels RGB da Imagem Segmentada: " + Arrays.toString(seg.getRegionMarkedPixels()));
         System.out.println( seg.getOriginalPixels().length + "Pixels RGB da Imagem Originalxx: " + Arrays.toString(seg.getOriginalPixels()));
-        System.out.println( seg.getSegmentedImageMap().length + "Mapa da Imagem Segmentada: " + Arrays.toString(seg.getSegmentedImageMap()));
-        
-        
-        int height = seg.getRegionMarkedImage().getHeight();
-        int width = seg.getRegionMarkedImage().getWidth();
-  
-        int[] pixel;
-        int ints[] = new int[seg.getRegionMarkedPixels().length];
-        ints = seg.getSegmentedImageMap();
-        
-        Color myWhite = new Color(255, 255, 255); // Color white
-        int rgb2=myWhite.getRGB();
-        
+        System.out.println( seg.getSegmentedImageMap().length + "Mapa da Imagem Segmentada: " + Arrays.toString(seg.getSegmentedImageMap()));        
         
         // Criação de um JFrame e inserção de 2 JLabels com cada uma das imagens.
         JFrame frame = new JFrame();
         frame.getContentPane().setLayout(new FlowLayout());
         frame.getContentPane().add(new JLabel(new ImageIcon(seg.getOriginalImage()))); // Imagem original
-        frame.getContentPane().add(new JLabel(new ImageIcon(seg.getRegionMarkedImage()))); // Imagem segmentada
-
-        int aux = 0;
-        ArrayList<Integer> elements = new ArrayList<>();       
+        frame.getContentPane().add(new JLabel(new ImageIcon(seg.getRegionMarkedImage()))); // Imagem segmentada             
         
         /**
-         * Retorna as coordenadas de cada pixel da imagem.
+         * Mapa de Rótulos...
+         * Precisamos determinar a cor dos pixels de cada região da imagem segmentada,
+         * A função abaixo retorna as coordenadas de cada pixel da imagem, além do seu RGB.
+         * Também define a cor do pixel através do parâmetro setRGB.
          */
 
         for(int y = 0; y < height; y++) {
             for(int x = 0; x < width; x++) {
-                pixel = seg.getRegionMarkedImage().getRaster().getPixel(x,y, new int[3]);                               
-                System.out.println(pixel[0] + " - " + pixel[1] + " - " + pixel[2]);
-                if(ints[x] == 9) {
-                    image.setRGB(x, y, rgb2);
-                    System.out.println("oks");
-                }
+                pixel = seg.getRegionMarkedImage().getRaster().getPixel(x,y, new int[3]);
+                //image.setRGB(x, y, rgb); // define a cor do pixel
+                System.out.println(pixel[0] + " - " + pixel[1] + " - " + pixel[2]);                
             }
         }
-       
-        frame.getContentPane().add(new JLabel(new ImageIcon(image)));      
-
-        
-        
+               
+        frame.getContentPane().add(new JLabel(new ImageIcon(image))); //mapa de rótulos :/      
+                
         frame.pack();
         frame.setVisible(true);
     }         
