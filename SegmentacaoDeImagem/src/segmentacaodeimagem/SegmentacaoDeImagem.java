536abd570b54378620b825682909ef9d12ccc9c7
Hiago Miguel <miguel.hiago@gmail.com>
2015-10-18 21:17:13 -0300

    Pequenas alterações

--- SegmentacaoDeImagem/src/segmentacaodeimagem/SegmentacaoDeImagem.java
+++ SegmentacaoDeImagem/src/segmentacaodeimagem/SegmentacaoDeImagem.java
@@ -15,6 +15,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.image.BufferedImage;
 
+
 /**
  *
  * @author hiago
@@ -40,15 +41,17 @@ public class SegmentacaoDeImagem {
         int width = seg.getRegionMarkedImage().getWidth(); // largura da imagem
   
         int[] pixel; // array que contém as coordenadas x,y e o valor rgb do pixel.
+        int[] mapaDaRegiaoSegmentada = seg.getSegmentedImageMap();
+        int[] pixelsDaImagemSegmentada = seg.getRegionMarkedPixels();
         
         Color grey = new Color(121, 121, 121); // uma cor para teste
         int rgb = grey.getRGB();
 
         // Impressão na tela da quantidade de regiões gerada
         System.out.println("Total de regiões: " + seg.getTotalRegions());
-        System.out.println( seg.getRegionMarkedPixels().length + "Pixels RGB da Imagem Segmentada: " + Arrays.toString(seg.getRegionMarkedPixels()));
-        System.out.println( seg.getOriginalPixels().length + "Pixels RGB da Imagem Originalxx: " + Arrays.toString(seg.getOriginalPixels()));
-        System.out.println( seg.getSegmentedImageMap().length + "Mapa da Imagem Segmentada: " + Arrays.toString(seg.getSegmentedImageMap()));        
+        System.out.println("Tamanho: " + seg.getRegionMarkedPixels().length + " | Pixels RGB da Imagem Segmentada: " + Arrays.toString(seg.getRegionMarkedPixels()));
+        System.out.println("Tamanho: " + seg.getOriginalPixels().length + " | Pixels RGB da Imagem Originalxx: " + Arrays.toString(seg.getOriginalPixels()));
+        System.out.println("Tamanho: " + seg.getSegmentedImageMap().length + " | Mapa da Imagem Segmentada: " + Arrays.toString(seg.getSegmentedImageMap()));        
         
         // Criação de um JFrame e inserção de 2 JLabels com cada uma das imagens.
         JFrame frame = new JFrame();
@@ -58,11 +61,15 @@ public class SegmentacaoDeImagem {
         
         /**
          * Mapa de Rótulos...
-         * Precisamos determinar a cor dos pixels de cada região da imagem segmentada,
+         * Precisamos determinar a cor dos pixels de cada região da imagem segmentada.
+         * As regiões podem ser determinadas através do array retornado pelo método seg.getSegmentedImageMap(). 
+         * Cada região tem um número fixo que está associado a cada pixel da imagem de forma sequencial (não tenho certeza),
+         * basta comparar os arrays seg.getRegionMarkedPixels() e seg.getSegmentedImageMap(), eles possuem o mesmo tamanho.
          * A função abaixo retorna as coordenadas de cada pixel da imagem, além do seu RGB.
          * Também define a cor do pixel através do parâmetro setRGB.
          */
 
+        
         for(int y = 0; y < height; y++) {
             for(int x = 0; x < width; x++) {
                 pixel = seg.getRegionMarkedImage().getRaster().getPixel(x,y, new int[3]);
@@ -70,10 +77,26 @@ public class SegmentacaoDeImagem {
                 System.out.println(pixel[0] + " - " + pixel[1] + " - " + pixel[2]);                
             }
         }
-               
-        frame.getContentPane().add(new JLabel(new ImageIcon(image))); //mapa de rótulos :/      
-                
+        
+/*        
+        int regiao = 0;
+        int tmp = 0;
+
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                pixel = seg.getRegionMarkedImage().getRaster().getPixel(x,y, new int[3]);
+                if(mapaDaRegiaoSegmentada[x] == 9) {
+                    seg.getRegionMarkedImage().setRGB(x, y, rgb);
+                }
+                System.out.println("Working? " + x + " - " + " - " + y + " - " + rgb);
+            }
+        }                
+  */     
+
+        frame.getContentPane().add(new JLabel(new ImageIcon(seg.getRegionMarkedImage()))); //mapa de rótulos :/      
+        
         frame.pack();
         frame.setVisible(true);
-    }         
+    }
+    
 }
