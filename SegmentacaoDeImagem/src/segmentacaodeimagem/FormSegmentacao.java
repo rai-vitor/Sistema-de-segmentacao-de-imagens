47b94a94c066eace6c0774931dcb837a00a23347
Rai Vitor Morais da Silva <raivitor@gmail.com>
2015-10-22 17:05:58 -0300

    Correção do doc

--- SegmentacaoDeImagem/src/segmentacaodeimagem/FormSegmentacao.java
+++ SegmentacaoDeImagem/src/segmentacaodeimagem/FormSegmentacao.java
@@ -247,10 +247,9 @@ public class FormSegmentacao extends javax.swing.JFrame {
         repaint();
     }
     /**
-     * 
+     * Método que inicia o sistema
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -272,7 +271,6 @@ public class FormSegmentacao extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(FormSegmentacao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new FormSegmentacao().setVisible(true);
