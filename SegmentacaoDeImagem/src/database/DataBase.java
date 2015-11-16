3814b3663389f8aae8a69a3ac615b4b6ab991d2f
Rai Vitor Morais da Silva <raivitor@gmail.com>
2015-11-16 16:50:44 -0300

    Removendo métodos de Database não utilizados

--- SegmentacaoDeImagem/src/database/DataBase.java
+++ SegmentacaoDeImagem/src/database/DataBase.java
@@ -2,8 +2,6 @@ package database;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import segmentacaodeimagem.Anotacao;
 import segmentacaodeimagem.Imagem;
 import segmentacaodeimagem.ListAnotacoes;
@@ -65,12 +63,10 @@ public class DataBase {
         stmt = null;
         try {
             conexao = DriverManager.getConnection(banco);
-            conexao.setAutoCommit(false);
-
             stmt = conexao.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM IMG WHERE PATHIMG = '" + pathImg + "';");
 
-          //Se selecionar algo retorna o id
+            //Se selecionar algo retorna o id
             //se não encontrar nada retorno -1
             int id = -1;
             while (rs.next()) {
@@ -197,86 +193,15 @@ public class DataBase {
         stmt = null;
         try {
             conexao = DriverManager.getConnection(banco);
-            conexao.setAutoCommit(false);
-
             stmt = conexao.createStatement();
             String sql = "INSERT INTO ANOTACAO (TAG,REGIAO,ID_IMG_FK) VALUES ('" + tag + "', '" + regiao + "', '" + imgId + "');";
             stmt.executeUpdate(sql);
-
-            conexao.commit();
+            
             stmt.close();
             conexao.close();
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
-    }
-
-    /**
-     * Seleciona todas as imagens no banco.
-     *
-     * @return retorna o caminho e o id das imagens no banco.
-     */
-    public Map<Integer, String> ListarImg() {
-        conexao = null;
-        stmt = null;
-        Map<Integer, String> mapImg = new HashMap<>();
-
-        try {
-            conexao = DriverManager.getConnection(banco);
-            conexao.setAutoCommit(false);
-
-            stmt = conexao.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM IMG;");
-
-            while (rs.next()) {
-                int id = rs.getInt("ID_IMG");
-                String path = rs.getString("PATHIMG");
-                System.out.println("ID = " + id);
-                System.out.println("PATHIMG = " + path);
-                System.out.println();
-                mapImg.put(id, path);
-            }
-            rs.close();
-            stmt.close();
-            conexao.close();
-            return mapImg;
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-        return null;
-    }
-
-    /**
-     * Lista todas as anotações do banco.
-     *
-     * @return lista com todas as anotações.
-     */
-    public ArrayList<Anotacao> ListarAnotacoes() {
-        conexao = null;
-        stmt = null;
-        try {
-            conexao = DriverManager.getConnection(banco);
-            conexao.setAutoCommit(false);
-            ArrayList<Anotacao> listaAnotacao = new ArrayList<>();
-            stmt = conexao.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM ANOTACAO INNER JOIN IMG ON ANOTACAO.ID_IMG_FK = IMG.ID_IMG;");
-            while (rs.next()) {
-                String tag = rs.getString("TAG");
-                int regiao = rs.getInt("REGIAO");
-                String pathImg = rs.getString("PATHIMG");
-                Anotacao note = new Anotacao(pathImg, tag, regiao);
-                listaAnotacao.add(note);
-            }
-            rs.close();
-            stmt.close();
-            conexao.close();
-            return listaAnotacao;
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-        return null;
     }
 
     /**
@@ -309,28 +234,6 @@ public class DataBase {
     }
 
     /**
-     * Deleta uma imagem no banco.
-     *
-     * @param idImg - id da imagem a ser deletada.
-     */
-    public void DeletarImg(int idImg) {
-        try {
-            conexao = DriverManager.getConnection(banco);
-            conexao.setAutoCommit(false);
-
-            stmt = conexao.createStatement();
-            String sql = "DELETE FROM ANOTACAO WHERE ID_IMG_FK = '" + idImg + "';";
-            stmt.executeUpdate(sql);
-
-            conexao.commit();
-            stmt.close();
-            conexao.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
-    }
-
-    /**
      * Deleta uma anotação no banco.
      *
      * @param note - anotação a ser deletada.
@@ -342,13 +245,10 @@ public class DataBase {
         }
         try {
             conexao = DriverManager.getConnection(banco);
-            conexao.setAutoCommit(false);
-
             stmt = conexao.createStatement();
             String sql = "DELETE FROM ANOTACAO WHERE TAG = '" + note.getTag() + "' AND REGIAO = '" + note.getRegiao() + "' AND ID_IMG_FK = '" + idImg + "';";
             stmt.executeUpdate(sql);
 
-            conexao.commit();
             stmt.close();
             conexao.close();
         } catch (Exception e) {
@@ -367,27 +267,10 @@ public class DataBase {
             conexao = DriverManager.getConnection(banco);
             stmt = conexao.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM ANOTACAO;");
-/*
-            tags.clear();
-            imgs.clear();*/
+
             Trie t = new Trie();
             while (rs.next()) {
-                
                 String tag = rs.getString("TAG");
-                /*
-                int regiao = rs.getInt("REGIAO");
-                String pathImg = rs.getString("PATHIMG");
-                Anotacao note = new Anotacao(pathImg, tag, regiao);
-                tags.add(note);
-                
-                if (!imgs.contains(pathImg)) { // para não ter paths repetidos
-                    double blur = rs.getDouble("BLUR");
-                    int radius = rs.getInt("RADIUS");
-                    int size = rs.getInt("SIZE");
-                    Imagem img = new Imagem(pathImg,blur, radius, size);
-                    imgs.add(img);
-                }
-                */
                 t.insert(tag); // add na arvore trie
             }
 
